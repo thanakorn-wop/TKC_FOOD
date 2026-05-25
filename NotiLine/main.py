78a27cb28e2570fb63f8d3d9166d611e78a825f9
thanakorn-wop <64125456+thanakorn-wop@users.noreply.github.com>
2026-05-25 11:11:02 +0700

    Add files via upload

---
+++ NotiLine/main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, Request
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
+
+app = FastAPI()
+
+ACCESS_TOKEN = "b2OWovv+Tb1RFeG4cfna2VoqvyiKxveehmq71P/Iz5bSzkPK+HobwYARprfmHXrXo5SnWdC7diDITbt4C2PvQzR/vShEDKiae1BY5y7TgY8ZaaEtxoJTRXoBdu/SI3H0zEBwpUakpNIQpA9Uts+4UwdB04t89/1O/w1cDnyilFU="
+
+USER_ID = "e083ff7ce8eea0a44d73028b3eb89a01"
+
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(USER_ID)
+
+
+class Product:
+
+    def __init__(self, name, amount,in_product,exp_date):
+
+        self.name = name
+        self.amount = amount
+        self.in_product = in_product
+        self.exp_date = exp_date
+
+    # def add_stock(self, qty):
+
+    #     self.stock += qty
+
+    # def remove_stock(self, qty):
+
+    #     self.stock -= qty
+
+    def show(self):
+        print(self.name, self.amount, self.in_product, self.exp_date)
+
+
+@app.post("/callback")
+async def callback(request: Request):
+
+    signature = request.headers["X-Line-Signature"]
+
+    body = await request.body()
+    body = body.decode("utf-8")
+
+    try:
+        handler.handle(body, signature)
+
+    except InvalidSignatureError:
+        return {"status": "invalid signature"}
+
+    return {"status": "ok"}
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    user_text = event.message.text
+
+    print("ข้อความที่ได้รับ:", user_text)
+    if(user_text.lower().strip() == "stock" or user_text.upper().strip() == "STOCK" or user_text.strip() == "สต๊อก"):
+        user_text = "stock วันนี้?"
+        productt    = Product("สินค้า A", 100, "2023-12-31", "2023-12-31")
+    
+    list_product = f"-------รายการสินค้า-------"+f"\nชื่อสินค้า: {productt.name}\nจำนวน: {productt.amount}\nวันที่รับเข้า: {productt.in_product}\nวันหมดอายุ: {productt.exp_date}"
+    user_text = list_product
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(
+            text=user_text
+        )
+    )
